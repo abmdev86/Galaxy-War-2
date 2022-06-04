ee497324143a16a46e31e9c3ba405dbfca85ab31
Auroiah Morgan <amwebdev@outlook.com>
2022-06-03 23:32:20 -0500

    Add Enemy Spawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using com.sluggagames.gw2.Core;
+using com.sluggagames.gw2.Core.Interfaces;
 using UnityEngine;
 
 namespace com.sluggagames.gw2.Enemy
@@ -17,9 +19,32 @@ namespace com.sluggagames.gw2.Enemy
 
         GameObject enemies;
 
+        private void Awake()
+        {
+            enemies = GameObject.Find("_enemies");
+            StartCoroutine(FireEnemy(quantity, spawnRate));
+        }
 
+        IEnumerator FireEnemy(int qty, float spwnRate)
+        {
+            for (int i = 0; i < qty; i++)
+            {
+                GameObject enemyUnit = CreateEnemy();
+                enemyUnit.gameObject.transform.SetParent(this.transform);
+                enemyUnit.transform.position = transform.position;
+                yield return new WaitForSeconds(spwnRate);
+            }
+            yield return null;
 
+        }
 
-
+        private GameObject CreateEnemy()
+        {
+            GameObject enemy = GameObject.Instantiate(actorModel.actor) as GameObject;
+            var template = enemy.GetComponent<IActorTemplate>();
+            template.ActorStats(actorModel);
+            enemy.name = actorModel.actorName.ToString();
+            return enemy;
+        }
     }
 }
