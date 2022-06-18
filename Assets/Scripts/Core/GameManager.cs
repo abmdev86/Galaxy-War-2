11f1ea87554c938ad6de1ee6fa086b904b960e8d
Auroiah Morgan <amwebdev@outlook.com>
2022-06-18 12:12:15 -0500

    Updates the camera

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace com.sluggagames.gw2.Core
 {
-    public  class GameManager : MonoBehaviour
+    public class GameManager : MonoBehaviour
     {
         static GameManager instance;
         public static GameManager Instance
@@ -16,14 +16,20 @@ namespace com.sluggagames.gw2.Core
             }
         }
 
-    LevelManager sceneManager;
-    ScoreManager scoreManager;
+        LevelManager sceneManager;
+        ScoreManager scoreManager;
 
         public static int currentScene = 0;
         public static int gameLevelScene = 3;
 
+        [SerializeField]
+        bool isDebug = false;
+        [Range(-200, 200)]
+        public float cameraStartingDistanceX, cameraStartingDistanceY, cameraStartingDistanceZ;
+        [Range(0, 360)]
+        public float debugEulerAngleY = 0;
         public static int playerLives = 3;
-        CinemachineVirtualCamera cam;
+        //  CinemachineVirtualCamera cam;
         bool died = false;
         public bool Died
         {
@@ -33,19 +39,23 @@ namespace com.sluggagames.gw2.Core
 
         void Awake()
         {
-            cam = (CinemachineVirtualCamera)GameObject.FindObjectOfType(typeof(CinemachineVirtualCamera));
+            // cam = (CinemachineVirtualCamera)GameObject.FindObjectOfType(typeof(CinemachineVirtualCamera));
             CheckGameManager();
-            
+
             sceneManager = GetComponent<LevelManager>();
             scoreManager = GetComponent<ScoreManager>();
-            
-            
-           
 
             // change to a call to the levelmanager to get this ....
             currentScene = SceneManager.GetActiveScene().buildIndex;
             currentScene = sceneManager.GetCurrentSceneIndex();
+            LightAndCameraSetup(gameLevelScene);
+
         }
+
+        // private void Start()
+        // {
+        //     LightAndCameraSetup(gameLevelScene);
+        // }
 
         void LightAndCameraSetup(int sceneIndex)
         {
@@ -58,6 +68,7 @@ namespace com.sluggagames.gw2.Core
                     {
                         LightSetup();
                         CameraSetup();
+
                         break;
                     }
             }
@@ -65,27 +76,46 @@ namespace com.sluggagames.gw2.Core
 
         private void CameraSetup()
         {
+            var camera = Camera.main;
             var player = GameObject.Find("_player");
+            if (isDebug)
+            {
+                camera.transform.position = new Vector3(cameraStartingDistanceX, cameraStartingDistanceY, cameraStartingDistanceZ);
+                camera.transform.eulerAngles = new Vector3(0, debugEulerAngleY, 0);
+            }
+            else
+            {
+                camera.transform.position = new Vector3(40, 2, 1);
+                camera.transform.eulerAngles = new Vector3(0, -90, 0);
+            }
+
+
+
         }
 
-        public void LifeLost(){
-            if(playerLives >=1){
-            playerLives--;
-            print($"Lives remaining {playerLives}");
-           sceneManager.ResetScene();
-            }else{
+        public void LifeLost()
+        {
+            if (playerLives >= 1)
+            {
+                playerLives--;
+                print($"Lives remaining {playerLives}");
+                sceneManager.ResetScene();
+            }
+            else
+            {
                 playerLives = 3;
                 sceneManager.GameOver();
             }
         }
 
-        public void SetScore(int value){
-             scoreManager.SetScore(value);
-            }
+        public void SetScore(int value)
+        {
+            scoreManager.SetScore(value);
+        }
 
         private void LightSetup()
         {
-            throw new NotImplementedException();
+            Debug.Log("LightSetup()");
         }
 
         void CheckGameManager()
